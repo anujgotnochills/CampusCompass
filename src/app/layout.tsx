95d673c9156022e5578910bfd61d908db0941569
anuj chouhan <wwwanujchouhan@gmail.com>
2025-09-09 15:26:15 +0000

    https://sss.matchboxstudio.com/
    use lenis for smooth scolling and make

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
 
+"use client";
+
 import type { Metadata } from 'next';
 import { Inter, Outfit } from 'next/font/google';
 import './globals.css';
 import { AppProvider } from '@/contexts/AppContext';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
+import { ReactLenis } from '@studio-freight/react-lenis'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 const outfit = Outfit({ subsets: ['latin'], weight: ['700'], variable: '--font-outfit' });
 
 
-export const metadata: Metadata = {
-  title: 'Campus Compass - Lost & Found',
-  description: 'Helping students find their lost items on campus.',
-};
+// export const metadata: Metadata = {
+//   title: 'Campus Compass - Lost & Found',
+//   description: 'Helping students find their lost items on campus.',
+// };
 
 export default function RootLayout({
   children,
@@ -22,11 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning style={{ scrollBehavior: 'smooth' }} className="dark">
-      <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable, outfit.variable)}>
-        <AppProvider>
-          {children}
-          <Toaster />
-        </AppProvider>
+       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable, outfit.variable)}>
+        <ReactLenis root>
+          <AppProvider>
+            {children}
+            <Toaster />
+          </AppProvider>
+        </ReactLenis>
       </body>
     </html>
   );
