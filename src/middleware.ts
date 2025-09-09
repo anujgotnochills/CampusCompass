2be3268c6eae414e3b00862f80f5f1c88fde64f8
anuj chouhan <wwwanujchouhan@gmail.com>
2025-09-09 14:20:01 +0000

    i want you to bypass login from now so that i can start  devopl inside d

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,10 +17,10 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL('/dashboard', req.url))
   }
 
-  // if user is not signed in and the current path is not a public page, redirect the user to /
-  if (!user && !['/', '/login', '/signup'].includes(req.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
+  // if user is not signed in and the current path is not a public page, redirect the user to /login
+  // if (!user && !['/', '/login', '/signup'].includes(req.nextUrl.pathname)) {
+  //   return NextResponse.redirect(new URL('/login', req.url))
+  // }
   
   await supabase.auth.getSession()
 
