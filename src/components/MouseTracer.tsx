1b3cc3e49d7fb00909b075b202f88b212d2d1c75
anuj chouhan <wwwanujchouhan@gmail.com>
2025-09-12 14:30:22 +0000

    while opening the app on new tab the bugs are coming like mouse tracking

--- src/components/MouseTracer.tsx
+++ src/components/MouseTracer.tsx
@@ -30,7 +30,7 @@ export function MouseTracer() {
 
   return (
     <div 
-      className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+      className="pointer-events-none fixed inset-0 z-30 transition duration-300"
       style={{
         background: `radial-gradient(600px at ${position.x}px ${position.y}px, rgba(244, 114, 182, 0.15), transparent 80%)`
       }}
